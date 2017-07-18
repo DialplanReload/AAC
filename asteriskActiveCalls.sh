df65f7a38b56c0d8b23750e75cf884393af4dd83
Anderson Freitas <tmsi.freitas@gmail.com>
2017-07-18 11:08:17 -0300

    Create asteriskActiveCalls.sh
    Add archive asteriskActiveCalls.sh to colect simultaneous calls on asterisk. V1.0

---
+++ asteriskActiveCalls.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# asteriskActiveCalls.sh - Programa feito para checar a quantidade de chamadas simultaneas
+# do Asterisk e bilhetar dentro de um arquivo
+#
+# Autor: Anderson Freitas <tmsi.freitas@gmail.com>
+# Site: http://www.dialplanreload.com/
+# Repositorio: https://github.com/DialplanReload/AAC
+#
+# Desenvolvido sob licensa GPL. 
+# Fique a vontade para contribuir com a evolucao deste programa.
+#
+#-----------------------------------------------------------------------------------------------
+#
+#
+# Execucao simples - ./asteriskActiveCalls.sh
+#
+is_number() {
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]]; then
+        echo 0
+    fi
+
+    echo 1
+}
+
+calls=$(rasterisk -x "core show channels verbose" | grep "active call" | awk '{print $1}' && date)
+
+if [ -z $calls ]; then
+    echo -1
+    exit -1
+fi
+
+if [ $(is_number $calls) -gt 0 ]
+then
+    echo $calls
+    exit 0
+else
+    echo -1
+    exit -1
+fi
+
+echo $calls >> callsHistory.md
